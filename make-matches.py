31db9a6acc9ca8823f2ee1de3740cb0cb402a4ff
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-05-15 08:33:39 +0100

    refactor: romantic and platonic net_id

--- make-matches.py
+++ make-matches.py
@@ -30,12 +30,12 @@ if google_sheet_input:
 		hail_mary_column = st.text_input('Enter the column name with the hail mary option', 'The Hail Mary')
 
 		# Indexing data for fast lookup
-		index_data = {row[netid_column]: row for index, row in data.iterrows()}
+		netid_to_row = {row[netid_column]: row for index, row in data.iterrows()}
 
 		# Dictionary to store matches
 		display_data = []
 
-		for netid, row in index_data.items():
+		for netid, row in netid_to_row.items():
 			romantic_matches = []
 			platonic_matches = []
 
@@ -43,10 +43,10 @@ if google_sheet_input:
 			try:
 				romantic_netids = row[romantic_matches_column].split('\n')
 				romantic_matches = [{
-						'NetID': match,
-						'Name': index_data[match][name_column],
-						'Email': index_data[match][email_column]
-				} for match in romantic_netids if match in index_data and netid in index_data.get(match, {}).get(romantic_matches_column, '').split('\n')]
+						'NetID': romantic_netid,
+						'Name': netid_to_row[romantic_netid][name_column],
+						'Email': netid_to_row[romantic_netid][email_column]
+				} for romantic_netid in romantic_netids if romantic_netid in netid_to_row and netid in netid_to_row.get(romantic_netid, {}).get(romantic_matches_column, '').split('\n')]
 			except AttributeError:
 				romantic_matches = []
 
@@ -54,10 +54,10 @@ if google_sheet_input:
 			try:
 				platonic_netids = row[platonic_matches_column].split('\n')
 				platonic_matches = [{
-						'NetID': match,
-						'Name': index_data[match][name_column],
-						'Email': index_data[match][email_column]
-				} for match in platonic_netids if match in index_data and netid in index_data.get(match, {}).get(platonic_matches_column, '').split('\n')]
+						'NetID': platonic_netid,
+						'Name': netid_to_row[platonic_netid][name_column],
+						'Email': netid_to_row[platonic_netid][email_column]
+				} for platonic_netid in platonic_netids if platonic_netid in netid_to_row and netid in netid_to_row.get(platonic_netid, {}).get(platonic_matches_column, '').split('\n')]
 			except AttributeError:
 				platonic_matches = []
 
